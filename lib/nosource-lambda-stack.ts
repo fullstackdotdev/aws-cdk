3c310db77018aeab78d4ae88598f494b60d581bc
Shirish Munukuntla <shirish.munukuntla@fullstack.dev>
2021-01-07 22:32:48 -0500

    removed unwanted comments

--- lib/nosource-lambda-stack.ts
+++ lib/nosource-lambda-stack.ts
@@ -5,11 +5,10 @@ export class NoSourceLambdaStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // defines an AWS Lambda resource
     new lambda.Function(this, 'SourceIsUnknown', {
-      runtime: lambda.Runtime.NODEJS_12_X, // execution environment
-      code: lambda.Code.fromAsset('lib/nosource-lambda/dist'), // code loaded from "lambda" directory
-      handler: 'index.handler' // file is "hello", function is "handler"
+      runtime: lambda.Runtime.NODEJS_12_X,
+      code: lambda.Code.fromAsset('lib/nosource-lambda/dist'),
+      handler: 'index.handler'
     });
   }
 }
